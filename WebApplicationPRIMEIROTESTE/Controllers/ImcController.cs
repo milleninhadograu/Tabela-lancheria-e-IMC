41d3404cb892a644039d873a118faca559ff1db4
millena do grau <00mk2080@gmail.com>
2023-05-10 23:15:18 -0300

    first commit

---
+++ WebApplicationPRIMEIROTESTE/Controllers/ImcController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationPRIMEIROTESTE.Controllers
+{
+    public class ImcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lancheria()
+        {
+            return View();
+        }
+    }
+}
